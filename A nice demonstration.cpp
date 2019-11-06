c07eedd396946435cb9b13c41556bd3e763a84c6
Awnon Bhowmik <awnonbhowmik@users.noreply.github.com>
2019-11-06 09:47:52 -0500

    Create A nice demonstration.cpp

---
+++ A nice demonstration.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int count = 0;
+
+int BinarySearch(vector<int>vec, int low, int high, int x)
+{
+	while (low <= high)
+	{
+		int mid = (low + high) / 2;
+    cout<<"central element = "<<vec[mid]<<"\n";
+		if (x > vec[mid]){
+      ::count++;
+      for(int i=mid+1;i<=high;i++)
+        cout<<vec[i]<<" ";
+        cout<<"\n";
+			return BinarySearch(vec, mid + 1, high, x);
+    }
+		else if (x < vec[mid]){
+      for(int i=low;i<=mid-1;i++)
+        cout<<vec[i]<<" ";
+        cout<<"\n";
+      ::count++;
+			return BinarySearch(vec, low, mid - 1, x);
+    }
+		else{
+      ::count++;
+			return ::count;
+    }
+	}
+
+	return -1;
+}
+
+int main() {
+  vector<int>a={2,-4,6,10,12,-7,8,0,22};
+  sort(a.begin(),a.end());
+
+  for(auto x:a)
+    cout<<x<<" ";
+  int low = 0;
+  int high = a.size()-1;
+
+  cout<<"\n\nFind 0:\n\n"<<BinarySearch(a,low,high,0);
+}
